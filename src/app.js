7501cf9a8860b8c5ec772b7be86cdb63dbb7a772
Kirill Belov <showc7@gmail.com>
2016-11-13 20:52:21 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+//import 'zone/lib/zone.js';
 import { Component } from 'angular2/core';
+import { Header } from './header.js';
 
 @Component({
   selector: 'my-app',
   template: `
+    <my-header></my-header>
     <h1>Welcome to your Angular2 + Webpack + ES6 Playground</h1>
     <h2>Have {{ what }}</h2>
   `
---
+++ src/header.js
@@ -0,0 +1,13 @@
+import { Component, OnInit } from 'angular2/core';
+
+@Component({
+  selector: 'my-header',
+  template: `
+    <h1>HEADER</h1>
+  `
+})
+export default class Header {
+  constructor() {
+
+  }
+};
